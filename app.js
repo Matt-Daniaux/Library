1c2ba93752eb475c20fa2ea5c808bd954a73c39d
Matt-Daniaux <matthieudaniaux@gmail.com>
2023-02-11 12:49:12 +0100

    Add basic file structure

---
+++ app.js
@@ -0,0 +1,18 @@
+const myLibrary = []
+
+function Book(title, author, pages, read) {
+	this.title = title
+	this.author = author
+	this.pages = pages
+	this.read = read
+	this.info = function () {
+		return `${title} by ${author}, ${pages} pages, ${read}`
+	}
+}
+
+function addBookToLibrary(title, author, pages, read) {
+	const book = new Book(title, author, pages, read)
+	myLibrary.push(book)
+}
+
+console.table(myLibrary)
